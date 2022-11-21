5bff493a1bed0f5464e198c49df94794c5b5a389
Lorenzo Prati <lorenzo.prati3@studio.unibo.it>
2022-11-21 17:01:56 +0100

    Solve checkstyle, pmd errors

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -3,16 +3,17 @@ package it.unibo.oop.workers02;
 import java.util.ArrayList;
 import java.util.List;
 
-import it.unibo.oop.workers01.MultiThreadedListSumClassic;
-
+/**
+ * Implements a class to calculate the sum of elements of a matrix.
+ */
 public class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int nthread;
 
     /**
-     * Builds a {@code Worker}
+     * Builds a {@code MultiThreadedSumMatrix}.
      * 
-     * @param nthreads is the no. of threads performing the sum
+     * @param nthread is the number of threads performing the sum
      */
     public MultiThreadedSumMatrix(final int nthread) {
         this.nthread = nthread;
@@ -22,21 +23,27 @@ public class MultiThreadedSumMatrix implements SumMatrix {
         private final double[][] matrix;
         private final int startpos;
         private final int nrow;
-        double res;
+        private double res;
 
+        /**
+         * 
+         * @param matrix is the matrix to sum
+         * @param startpos is the initial row for this worker
+         * @param nrow is the number of rows to use for this worker
+         */
         Worker(final double[][] matrix, final int startpos, final int nrow) {
             super();
-            this.matrix = matrix;
+            this.matrix = matrix.clone();
             this.startpos = startpos;
             this.nrow = nrow;
         }
 
         @Override
         public void run() {
-            System.out.println("Working from position " + startpos + " to position " + (startpos + nrow - 1));
+            System.out.println("Working from position " + startpos + " to position " + (startpos + nrow - 1)); // NOPMD
             res = 0;
             for (int i = startpos; i < startpos + nrow && i < matrix.length; i++) {
-                for (double d : matrix[i]) {
+                for (final double d : matrix[i]) {
                     res += d;
                 }
             }
@@ -59,8 +66,7 @@ public class MultiThreadedSumMatrix implements SumMatrix {
      * @return the sum of the elements of the matrix
      */
     @Override
-    public double sum(double[][] matrix) {
-
+    public double sum(final double[][] matrix) {
         final int size = matrix.length % nthread + matrix.length / nthread;
         final List<Worker> workers = new ArrayList<>(nthread);
         for (int start = 0; start < matrix.length; start += size) {
